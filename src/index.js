bda85bfb80e7e64c2a71f72ea7f289bf2bcade06
liuyukai <liuyukai@bizgo.com>
2024-11-21 18:20:10 +0800

    输入文字翻译

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const hover = require('./hover.js')
 const translate = require('./translate.js')
+const translateWord = require('./translateWord.js')
 function activate(context) {
     context.subscriptions.push(hover)
     context.subscriptions.push(translate)
+    context.subscriptions.push(translateWord)
 }
 
 module.exports = {
---
+++ src/translateWord.js
@@ -0,0 +1,23 @@
+const vscode = require("vscode");
+const fs= require("fs");
+const process = require("child_process");
+
+const path = vscode.window.activeTextEditor.document.uri.path.split('src')[0] + vscode.workspace.getConfiguration().get('i18n-txt-preview.trans')
+let fileContent = fs.readFileSync(path, 'utf-8')
+module.exports = vscode.commands.registerCommand('trans', () => {
+    vscode.window.showInputBox({
+        ignoreFocusOut: true, // 当焦点移动到编辑器的另一部分或另一个窗口时, 保持输入框打开
+        password: false, // 为 true 就表示是密码类型
+        prompt: "请输入需要翻译的文字", // 文本输入提示
+        value: "", // 默认值, 默认全部选中
+        valueSelection: [0, 0],  // 指定选中的范围
+    }).then(value => {
+        if (!value || !value?.trim()) {
+            vscode.window.showErrorMessage("你输入的文本无效");
+            return;
+        };
+        fileContent = fileContent.replace(new RegExp(/word = \[.*?\]/, 'g'), `word = ['${value}']`)
+        fs.writeFileSync(path, fileContent, 'utf-8')
+        process.exec(`node ${path}`);
+    })
+});
